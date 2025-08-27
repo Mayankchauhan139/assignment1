fdd52c896e84a68d67d7b221c14c2a7624a0e2ca
mayankchauhan139 <mayank.chauhan130904@gmail.com>
2025-08-28 01:02:49 +0530

    code uploaded

---
+++ task1.py
@@ -0,0 +1,6 @@
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+print("\nAddition:",num1+num2)
+print("Subtraction:",num1-num2)
+print("Multiplication:",num1*num2)
+print("Division:",num1/num2) if num2>0 else print("Division: Error! Division by zero is not allowed.")
