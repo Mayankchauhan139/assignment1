be54823a1d227120fff87cbcac542e642d8c269e
mayankchauhan139 <mayank.chauhan130904@gmail.com>
2025-08-28 22:06:50 +0530

    complete

--- task1.py
+++ task1.py
@@ -1,5 +1,7 @@
+#Taking input for opration
 num1=float(input("Enter the first number: "))
 num2=float(input("Enter the second number: "))
+# Performing operaations
 print("\nAddition:",num1+num2)
 print("Subtraction:",num1-num2)
 print("Multiplication:",num1*num2)
---
+++ task2.py
@@ -0,0 +1,4 @@
+#greeting program 
+name=input("Enter your first name: ")
+lastname=input("Enter your last name: ")
+print("\nHello, " + name + " " + lastname + "! Welcome to Python program.")
